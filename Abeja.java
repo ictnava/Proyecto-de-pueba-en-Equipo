20d3b670d87cb71bd8b94c26db3ccb92bf9f0a11
ictnava <ictorna@gmail.com>
2016-10-13 10:38:24 -0500

    Clase Abeja
    Se agrega la clase abeja

--- Abeja.java
+++ Abeja.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class butterfly here.
+ * Write a description of class Abeja here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class butterfly extends Actor
+public class Abeja extends Actor
 {
     /**
-     * Act - do whatever the butterfly wants to do. This method is called whenever
+     * Act - do whatever the Abeja wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
